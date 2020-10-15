d1665dc632b5dcf82aa5ec0e5c95aa559476ad5a
DploY707 <starj1024@gmail.com>
2020-10-15 19:51:52 +0900

    Implement basic method parse and make methodList which contains method's info & smali with bytecode

--- core/code_extractor.py
+++ core/code_extractor.py
@@ -1,4 +1,52 @@
 import os
+from collections import OrderedDict
+from androguard.core.bytecodes.apk import APK as openAPK
+from androguard.core.bytecodes.dvm import DalvikVMFormat as getDEX
+from androguard.core.analysis.analysis import Analysis as an
 
-def parse_methodList_from_APK(targetAPK, dataPath, resultPath) :
-    print("Parse Method Lists from APK")
+def method_info_to_dict(className, methodName, metaInfo, accessFlags, methodIndex, codeSize, instructions) :
+    dictString = OrderedDict()
+
+    dictString['className'] = className
+    dictString['methodName'] = methodName
+    dictString['metaInfo'] = metaInfo
+    dictString['accessFlags'] = accessFlags
+    dictString['methodIndex'] = methodIndex
+    dictString['codeSize'] = codeSize
+    dictString['instructions'] = instructions
+
+    return str(dictString)
+
+def get_instructions_from_method(method) :
+    instructions = ''
+
+    for n, inst in enumerate(method.get_instructions()) :
+        instructions = instructions + inst.disasm().replace('  ','') + '////'
+
+    return instructions
+
+def parse_methods_from_APK(targetAPK) :
+    apk = openAPK(targetAPK)
+    dex = getDEX(apk.get_dex())
+    methods = dex.get_methods()
+
+    return methods
+
+def generate_methodList(methods) :
+    methodInfoList = list()
+
+    for method in methods :
+        methodInfoList.append(method_info_to_dict(
+            method.get_class_name(),
+            method.get_name(),
+            method.get_information(),
+            method.get_access_flags_string(),
+            method.get_method_idx(),
+            method.get_length(),
+            get_instructions_from_method(method)
+            ))
+
+    print('Parse Method Lists from APK')
+
+    return methodInfoList
+
--- core/main.py
+++ core/main.py
@@ -1,16 +1,30 @@
-from code_extractor import parse_methodList_from_APK
 import sys, os
+from code_extractor import parse_methods_from_APK
+from code_extractor import generate_methodList
 
 APK_SET_PATH = '/root/workDir/data'
 RESULT_PATH = '/root/results/methodLists'
+
+def print_analyzing_status(index, dataSetSize, targetAPK) :
+    print('[' + str(index+1) + ' / ' + str(dataSetSize) + ']' +  ' Analyzing......  ' + targetAPK)
+
+def save_methodList(resultPath, contents) :
+    methodList = open(resultPath, 'w')
+    methodList.write(contents)
+    methodList.close()
 
 def generate_methodLists_from_dataSet(dataDir, resultDir) :
     dataSet = os.listdir(dataDir)
     apkNum = len(dataSet)
 
     for i in range(0, apkNum) :
-        print('[' + str(i+1) + ' / ' + str(apkNum) + ']' +  ' Analyzing......  ' + dataSet[i])
-        parse_methodList_from_APK(dataSet[i], dataDir, resultDir)
+        print_analyzing_status(i, apkNum, dataSet[i])
+
+        APKFilePath = dataDir + '/' + dataSet[i]
+        resultFilePath = resultDir + '/' + str(i) + '_' + dataSet[i]
+
+        #TODO : modify code for readability (easy to read)
+        save_methodList(resultFilePath, '\n'.join(generate_methodList(parse_methods_from_APK(APKFilePath))))
 
     print('Complete analyzing for ' + str(apkNum) + ' apks :)')
 
