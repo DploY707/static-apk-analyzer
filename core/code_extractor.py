09619cb430319726d78ec0e1aea3ac7d7c6c8940
DploY707 <starj1024@gmail.com>
2020-11-06 16:54:23 +0900

    Refactoring code extraction code, now all of the extraction sequence is managed by CodeExtractor Class

--- core/code_extractor.py
+++ core/code_extractor.py
@@ -2,115 +2,120 @@ import os
 from collections import OrderedDict
 from androguard.core.bytecodes.apk import APK as openAPK
 from androguard.core.bytecodes.dvm import DalvikVMFormat as getDEX
-from androguard.core.analysis.analysis import Analysis as an
 
-def method_info_to_dict(className, methodName, metaInfo, accessFlags, methodIndex, codeSize, instructions) :
-    methodDict = OrderedDict()
+class CodeExtractor :
 
-    methodDict['className'] = str(className)
-    methodDict['methodName'] = str(methodName)
-    methodDict['returnType'] = metaInfo['return']
-    methodDict['registers'] = metaInfo['registers']
-    methodDict['paramList'] = metaInfo['params']
-    methodDict['accessFlagList'] = accessFlags.split(' ')
-    methodDict['methodIndex'] = methodIndex
-    methodDict['codeSize'] = codeSize
-    methodDict['instructions'] = instructions
+    def __init__(self, target) :
+        self.apk = openAPK(target)
+        self.dex = getDEX(self.apk.get_dex())
+        self.methods = self.dex.get_methods()
 
-    return methodDict
+        self.methodInfoList = None
 
-def optimize_instruction_format(inst) :
-    if '-payload' not in inst :
-        instChars = list(inst)
+    def method_info_to_dict(self, className, methodName, metaInfo, accessFlags, methodIndex, codeSize, instructions) :
+        methodDict = OrderedDict()
 
-        splited = False
+        methodDict['className'] = str(className)
+        methodDict['methodName'] = str(methodName)
+        methodDict['returnType'] = metaInfo['return']
+        methodDict['registers'] = metaInfo['registers']
+        methodDict['paramList'] = metaInfo['params']
+        methodDict['accessFlagList'] = accessFlags.split(' ')
+        methodDict['methodIndex'] = methodIndex
+        methodDict['codeSize'] = codeSize
+        methodDict['instructions'] = instructions
 
-        optimized = ''
+        return methodDict
 
-        for i in range(len(instChars)) :
-            if not splited and str(instChars[i]).isspace() and str(instChars[i+1]).isspace() :
-                optimized += '||'
-                splited = True
+    def optimize_instruction_format(self, inst) :
+        if '-payload' not in inst :
+            instChars = list(inst)
+
+            splited = False
+
+            optimized = ''
+
+            for i in range(len(instChars)) :
+                if not splited and str(instChars[i]).isspace() and str(instChars[i+1]).isspace() :
+                    optimized += '||'
+                    splited = True
+                else :
+                    optimized += instChars[i]
+
+            optimized = (
+                    optimized
+                    .replace(' ', '')
+                    .replace(',', ', ')
+                    .split('||')
+                    )
+
+            dictInst = OrderedDict()
+
+            dictInst['bytecode'] = optimized[0]
+            dictInst['smali'] = optimized[1]
+
+            return dictInst
+
+        else :
+            return inst
+
+    def get_instructions_from_method(self, method) :
+        instructions = list()
+
+        for n, inst in enumerate(method.get_instructions()) :
+            instructions.append(self.optimize_instruction_format(inst.disasm()))
+
+        return instructions
+
+    def make_method_meta_info_inform(self, metaInfo) :
+        metaInfoDict = OrderedDict()
+        registerDict = OrderedDict()
+
+        if bool(metaInfo) :
+            metaInfoDict['return'] = str(metaInfo['return'])
+
+            if (-1) in metaInfo['registers'] :
+                metaInfoDict['registers'] = ''
             else :
-                optimized += instChars[i]
+                registerDict['begin'] = metaInfo['registers'][0]
+                registerDict['size'] = metaInfo['registers'][1] - metaInfo['registers'][0] + 1
+                metaInfoDict['registers'] = registerDict
 
-        optimized = (
-                optimized
-                .replace(' ', '')
-                .replace(',', ', ')
-                .split('||')
-                )
+            metaInfoDict['params'] = list()  # Params is a tuple-list
 
-        dictInst = OrderedDict()
+            if len(metaInfo) == 2 :
+                pass
+            elif len(metaInfo) == 3 :
+                for regNum, dataType in metaInfo['params'] :
+                    metaInfoDict['params'].append((regNum, str(dataType))) # Params is a tuple list
+            else :
+                print('meta info error')
 
-        dictInst['bytecode'] = optimized[0]
-        dictInst['smali'] = optimized[1]
-
-        return dictInst
-
-    else :
-        return inst
-
-def get_instructions_from_method(method) :
-    instructions = list()
-
-    for n, inst in enumerate(method.get_instructions()) :
-        instructions.append(optimize_instruction_format(inst.disasm()))
-
-    return instructions
-
-def parse_methods_from_APK(targetAPK) :
-    apk = openAPK(targetAPK)
-    dex = getDEX(apk.get_dex())
-    methods = dex.get_methods()
-
-    return methods
-
-def make_method_meta_info_inform(metaInfo) :
-    metaInfoDict = OrderedDict()
-    registerDict = OrderedDict()
-
-    if bool(metaInfo) :
-        metaInfoDict['return'] = str(metaInfo['return'])
-
-        if (-1) in metaInfo['registers'] :
+        else :
+            metaInfoDict['return'] = ''
             metaInfoDict['registers'] = ''
-        else :
-            registerDict['begin'] = metaInfo['registers'][0]
-            registerDict['size'] = metaInfo['registers'][1] - metaInfo['registers'][0] + 1
-            metaInfoDict['registers'] = registerDict
+            metaInfoDict['params'] = list()
 
-        metaInfoDict['params'] = list()  # Params is a tuple-list
+        return metaInfoDict
 
-        if len(metaInfo) == 2 :
-            pass
-        elif len(metaInfo) == 3 :
-            for regNum, dataType in metaInfo['params'] :
-                metaInfoDict['params'].append((regNum, str(dataType))) # Params is a tuple list
-        else :
-            print('meta info error')
+    def generate_methodList(self) :
+        self.methodInfoList = list()
 
-    else :
-        metaInfoDict['return'] = ''
-        metaInfoDict['registers'] = ''
-        metaInfoDict['params'] = list()
+        for method in self.methods :
+            self.methodInfoList.append(self.method_info_to_dict(
+                method.get_class_name(),
+                method.get_name(),
+                self.make_method_meta_info_inform(method.get_information()),
+                method.get_access_flags_string(),
+                method.get_method_idx(),
+                method.get_length(),
+                self.get_instructions_from_method(method)
+                ))
 
-    return metaInfoDict
+        print('Parse Method Lists from APK')
 
-def generate_methodList(methods) :
-    methodInfoList = list()
+    def get_methodInfoList(self) :
+        if self.methodInfoList is None :
+            self.generate_methodList()
 
-    for method in methods :
-        methodInfoList.append(method_info_to_dict(
-            method.get_class_name(),
-            method.get_name(),
-            make_method_meta_info_inform(method.get_information()),
-            method.get_access_flags_string(),
-            method.get_method_idx(),
-            method.get_length(),
-            get_instructions_from_method(method)
-            ))
-
-    print('Parse Method Lists from APK')
-
-    return methodInfoList
+        return self.methodInfoList
