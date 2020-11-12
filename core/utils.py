e30b9f97b1d446a9d8b7f810be523c42fea22d19
kordood <gigacms@gmail.com>
2020-11-12 04:24:30 -0800

    Create class DirStruct, the class is finding directory hierarchy structure

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,37 @@
 import os
 
-def get_fileList_from_directory(dirPath) :
-	fileList = os.listdir(dirPath)
-	return fileList
+class DirStruct:
+
+	def __init__(self, path) :
+		self.path = path
+
+	def get_fileList_from_directory(self, dirPath) :
+		return os.listdir(dirPath)
+
+	def get_directory_hierarchy(self) :
+		subDirPathList = list()
+
+		self.parse_directory_hierarchy(subDirPathList, self.path)
+		return subDirPathList
+
+	def parse_directory_hierarchy(self, dirPathList, dirPath) :
+		fileList = self.get_fileList_from_directory(dirPath)
+
+		for fileName in fileList :
+			filePath = dirPath + '/' + fileName
+
+			if self.is_directory(filePath) :
+				self.add_and_find_subDirectory(dirPathList, filePath)
+
+	def is_directory(self, dirPath) :
+		return os.path.isdir(dirPath)
+
+	def add_and_find_subDirectory(self, resultPathList, dirPath) :
+		resultPathList.append(dirPath)
+		self.parse_directory_hierarchy(resultPathList, dirPath)
+
+if __name__=="__main__" :
+	TEST_DIR_PATH = "/home/liberty/Code_Extractor/testDir"
+	f = DirStruct(TEST_DIR_PATH)
+	dirList = f.get_directory_hierarchy()
+	print(dirList)
