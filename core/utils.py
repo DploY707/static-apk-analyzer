1567b32b372af944bba6eb7de81b5e2a1975ae57
kordood <gigacms@gmail.com>
2020-11-12 21:41:25 -0800

    Implement get-method about directory which include any file.

--- core/utils.py
+++ core/utils.py
@@ -14,6 +14,25 @@ class DirStruct:
 		self.parse_directory_hierarchy(subDirPathList, self.path)
 		return subDirPathList
 
+	# Todo: make directoryList which is including APK(s)
+	def get_directory_include_file(self, dirPathList) :
+		resultDirList = list()
+
+		for dirPath in dirPathList :
+			fileList = self.get_fileList_from_directory(dirPath)
+
+			for fileName in fileList :
+				filePath = dirPath + '/' + fileName
+
+				if self.is_file(filePath) :
+					resultDirList.append(dirPath)
+					break
+
+		return resultDirList
+
+	# if useless : delete this method
+#def get_APKs_in_directory(self, dirPath) :
+
 	def parse_directory_hierarchy(self, dirPathList, dirPath) :
 		fileList = self.get_fileList_from_directory(dirPath)
 
@@ -26,12 +45,19 @@ class DirStruct:
 	def is_directory(self, dirPath) :
 		return os.path.isdir(dirPath)
 
+	def is_file(self, filePath) :
+		return os.path.isfile(filePath)
+
 	def add_and_find_subDirectory(self, resultPathList, dirPath) :
 		resultPathList.append(dirPath)
 		self.parse_directory_hierarchy(resultPathList, dirPath)
 
-if __name__=="__main__" :
+if __name__ == "__main__" :
+	# testcode of get_dir_include_file
 	TEST_DIR_PATH = "/home/liberty/Code_Extractor/testDir"
-	f = DirStruct(TEST_DIR_PATH)
-	dirList = f.get_directory_hierarchy()
-	print(dirList)
+	ds = DirStruct(TEST_DIR_PATH)
+	testDirList = ds.get_directory_hierarchy()
+	fileDirList = ds.get_directory_include_file(testDirList)
+	print(testDirList)
+	print(fileDirList)
+
