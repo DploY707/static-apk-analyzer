25cdc1b7f75f537907afb0e6c08a640a41f7eb10
kordood <gigacms@gmail.com>
2020-11-16 03:25:57 -0800

    Simplify save_methodList method, and resolve no such directory error to result file path out of the method.

--- core/main.py
+++ core/main.py
@@ -12,32 +12,34 @@ def print_progress_directories(index, totalDirList) :
 	print('\n[' + totalDirList[index] + '] in ' + str(totalDirList))
 
 def save_methodList(resultPath, methodInfoList) :
-	try :
-		methodList = open(resultPath, 'wb')
-	except FileNotFoundError :
-		make_missing_directory_from_filePath(resultPath)
-		methodList = open(resultPath, 'wb')
-
+	methodList = open(resultPath, 'wb')
 	pickle.dump(methodInfoList, methodList)
 	methodList.close()
 
-def make_missing_directory_from_filePath(filePath) :
-	os.makedirs(filePath.replace(filePath.split('/')[-1], ""))
-
 def generate_methodLists_from_dataSet(dataDir, resultDir) :
-	dataSet = get_only_files_in_directory(dataDir)
-	apkNum = len(dataSet)
+    dataSet = get_only_files_in_directory(dataDir)
+    apkNum = len(dataSet)
 
-	for i in range(0, apkNum) :
-		print_analyzing_status(i, apkNum, dataDir, dataSet[i])
+    for i in range(0, apkNum) :
+        print_analyzing_status(i, apkNum, dataDir, dataSet[i])
 
-		APKFilePath = dataDir + '/' + dataSet[i]
-		resultFilePath = resultDir + '/' + str(i) + '_' + dataSet[i]
+        APKFilePath = dataDir + '/' + dataSet[i]
+        resultFilePath = resultDir + '/' + str(i) + '_' + dataSet[i]
 
-		ce = CodeExtractor(APKFilePath)
-		save_methodList(resultFilePath, ce.get_methodInfoList())
+        ce = CodeExtractor(APKFilePath)
 
-	print('Complete analyzing for ' + str(apkNum) + ' apks :)')
+        if not is_savable_path(resultDir):
+            generate_midPoint_directory_to_endpoint(resultDir)
+
+        save_methodList(resultFilePath, ce.get_methodInfoList())
+
+    print('Complete analyzing for ' + str(apkNum) + ' apks :)')
+
+def is_savable_path(filePath) :
+    return os.path.exists(filePath)
+
+def generate_midPoint_directory_to_endpoint(endpointPath) :
+    os.makedirs(endpointPath)
 
 def get_only_files_in_directory(dirPath) :
 	fileList = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
@@ -63,7 +65,7 @@ if __name__ == '__main__' :
 	dataSetPathList, resultPathList = preprocess_directory_path(DATASET_ROOT_PATH, RESULT_ROOT_PATH)
 
 	for i in range(len(dataSetPathList)) :
-		print_progress_directorys(i, dataSetPathList)
+		print_progress_directories(i, dataSetPathList)
 		generate_methodLists_from_dataSet(dataSetPathList[i], resultPathList[i])
 
 
