36b51e8572b5baae91b0031a8bc20e6676f97896
kordood <gigacms@gmail.com>
2020-11-17 04:44:46 -0800

    Rebuild utils.py file. It supports get leafnode of input directory or sub-file list. + subprocess output to str, trim quote/newline

--- core/utils.py
+++ core/utils.py
@@ -1,56 +1,24 @@
-import os
+import os, subprocess
 
-class DirStruct:
+def get_fileList_in_directory(dirPath) :
+	return os.listdir(dirPath)
 
-	def __init__(self, path) :
-		self.path = path
+def get_leafNodes_in_directory(dirPath) :
+	findSubDir = subprocess.check_output("find " + dirPath + " -type d 2>/dev/null", shell=True)
+	resultStr = translate_subprocess_output_to_str(findSubDir)
+	resultPathList = trim_newLines(resultStr)[:-1]
 
-	def get_fileList_from_directory(self, dirPath) :
-		return os.listdir(dirPath)
+	return resultPathList
 
-	def get_all_subDirectory(self) :
-		subDirPathList = list()
+def translate_subprocess_output_to_str(outputByte) :
+	resultStr = str(outputByte)
+	resultStr = trim_quotation_marks(resultStr)[1]
 
-		self.parse_directory_hierarchy(subDirPathList, self.path)
-		return subDirPathList
+	return resultStr
 
-	# Todo: make directoryList which is including APK(s)
-	def get_directoryList_include_file(self, dirPathList) :
-		resultDirList = list()
+def trim_quotation_marks(inputStr) :
+	return inputStr.split("'")
 
-		for dirPath in dirPathList :
-			fileList = self.get_fileList_from_directory(dirPath)
+def trim_newLines(inputStr) :
+	return inputStr.split("\\n")
 
-			for fileName in fileList :
-				filePath = dirPath + '/' + fileName
-
-				if not self.is_directory(filePath) :
-					resultDirList.append(dirPath)
-					break
-
-		return resultDirList
-
-	def parse_directory_hierarchy(self, dirPathList, dirPath) :
-		fileList = self.get_fileList_from_directory(dirPath)
-
-		for fileName in fileList :
-			filePath = dirPath + '/' + fileName
-
-			if self.is_directory(filePath) :
-				self.add_and_find_subDirectory(dirPathList, filePath)
-
-	def is_directory(self, dirPath) :
-		return os.path.isdir(dirPath)
-
-	def add_and_find_subDirectory(self, resultPathList, dirPath) :
-		resultPathList.append(dirPath)
-		self.parse_directory_hierarchy(resultPathList, dirPath)
-
-if __name__ == "__main__" :
-	# testcode of get_dir_include_file
-	TEST_DIR_PATH = "/home/liberty/Code_Extractor/testDir"
-	ds = DirStruct(TEST_DIR_PATH)
-	testDirList = ds.get_directory_hierarchy()
-	fileDirList = ds.get_directory_include_file(testDirList)
-	print(testDirList)
-	print(fileDirList)
