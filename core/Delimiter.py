acc2b6a961355162fe7e1c87ca0c916713af0505
kordood <gigacms@gmail.com>
2020-12-01 23:59:18 -0800

    Move Delimiter class to file

---
+++ core/Delimiter.py
@@ -0,0 +1,16 @@
+# delimiter.py
+#
+# contributed by kordood
+
+from enum import Enum
+
+class Delimiter(Enum) :
+	PARENL = '('
+	PARENR = ')'
+	BRACEL = '{'
+	BRACER = '}'
+	DEFINE = 'define'
+	DECLARE = 'declare'
+	COMMA = ','
+	AT = '@'
+	PERCENT = '%'
